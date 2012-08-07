321e84600a5995c366e52ef6b5562fdda1f04813
Micah Carrick <micah@quixotix.com>
2012-08-07 09:16:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+AVR Microcontroller Tutorial
+============================
+
+This is a work in progress. You do not want to use this yet.
---
+++ blink-led/src/blink.c
@@ -0,0 +1,21 @@
+#define F_CPU 1000000UL     /* 1 MHz internal RC Oscillator */
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int
+main (void)
+{
+    DDRB |= (1 << DDB0); 
+    
+    while(1) 
+    {
+        PORTB |= _BV(PB0);
+        _delay_ms(500);
+        
+        PORTB &= ~_BV(PB0);                
+        _delay_ms(500);
+    }
+}
+     
+
